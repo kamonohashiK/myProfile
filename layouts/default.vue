f502f1481c990ca99a98f18fa845adeec4749bd3
門屋弘毅 <kamonohashiy@gmail.com>
2019-11-01 20:10:19 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+        <div class="text-center">
+        <a href="https://paypal.me/kamonohashikre?locale.x=ja_JP" target="_blank" rel="noopener noreferrer">
+          <v-btn>いくばくか支援</v-btn>
+        </a>
+        </div>
+      </v-container>
+    </v-content>
+    <v-footer
+      fixed
+      app
+    >
+    <v-spacer />
+      <span>&copy; 2019</span>
+    <v-spacer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Top',
+          to: '/'
+        },
+        {
+          icon: 'mdi-ferry',
+          title: 'Islands',
+          to: '/islands'
+        },
+      ],
+      title: 'かも屋'
+    }
+  }
+}
+</script>
+
+<style>
+  a {
+    text-decoration: none;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">
+        <v-avatar size="220">
+          <img src="/kamo.png" alt="John" />
+        </v-avatar>
+        <h1>Kouki Kadoya</h1>
+        <h3>週休7日で働きたい</h3>
+        <p>AWS / Python / Nuxt.js</p>
+        <div class="align-center">
+          <a href="https://twitter.com/platypus_k86" target="_blank">
+            <v-avatar size="64">
+              <img src="/Twitter.png" alt="John" />
+            </v-avatar>
+          </a>
+          <a href="https://www.facebook.com/kouki.kadoya" target="_blank">
+            <v-avatar size="64">
+              <img src="/Facebook.png" alt="John" />
+            </v-avatar>
+          </a>
+          <a href="https://github.com/kamonohashiK" target="_blank">
+            <v-avatar size="64">
+              <img src="/Github.png" alt="John" />
+            </v-avatar>
+          </a>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+};
+</script>
+
+<style>
+.v-avatar {
+  margin: 10px;
+}
+
+p {
+  margin: 10px 0;
+}
+</style>
