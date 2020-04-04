a38ff991c2fd4770fe0f5fc9fb97158d4d08379e
門屋弘毅 <kamonohashiy@gmail.com>
2020-04-04 22:53:30 +0900

    ブログの個別記事ページのたたき台を作成

--- pages/docs/_id.vue
+++ pages/docs/_id.vue
@@ -1,5 +1,42 @@
 <template>
-	<div>
-		<h2>タイトル</h2>
-	</div>
+  <div class="text-center">
+    <h2>{{ doc.title }}</h2>
+    <div>
+      <v-chip v-for="tag in doc.tags" :key="tag.id" class="ma-2">
+        {{ tag.tag_name }}
+      </v-chip>
+    </div>
+    <div v-html="doc.content" id="content"></div>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      doc: {},
+    };
+  },
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      process.env.CMS_URL + "/docs/" + params.id,
+      {
+        headers: { "X-API-KEY": process.env.CMS_API_KEY },
+      }
+    );
+    return {
+      doc: data,
+    };
+  },
+};
+</script>
+
+<style>
+#content {
+  text-align: left;
+  margin: 0 auto;
+  width: 80%;
+}
+</style>
