60311961ff0006143feab7545ad15e0d1a04e7ae
門屋弘毅 <kamonohashiy@gmail.com>
2020-04-04 23:46:09 +0900

    記事一覧から個別ページへのリンクを追加

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -3,17 +3,18 @@
     <v-row>
       <v-col cols="12" md="4" sm="6" v-for="doc in docs" :key="doc.title">
         <v-card class="mx-auto" max-width="344" outlined>
-          <v-img height="220" width="344" src="hoge"></v-img>
+          <nuxt-link :to="`/docs/${doc.id}`">
+            <v-img
+              height="220"
+              width="344"
+              :src="image(doc.tags[0].id)"
+            ></v-img>
+          </nuxt-link>
           <v-list-item three-line>
             <v-list-item-content>
               <v-list-item-title class="headline mb-1">{{
                 doc.title
               }}</v-list-item-title>
-              <!--
-              <v-list-item-subtitle
-                v-html="item.description"
-              ></v-list-item-subtitle>
-			  -->
               <v-card-text>
                 <v-chip-group column>
                   <v-chip v-for="tag in doc.tags" :key="tag.id" class="ma-2">
@@ -45,6 +46,21 @@ export default {
     return {
       docs: data.contents,
     };
+  },
+  methods: {
+    image(tag) {
+      switch (tag) {
+        case "vue":
+          return "vue.png";
+          break;
+        case "nuxt":
+          return "nuxt.png";
+          break;
+        default:
+          return "http://placehold.jp/eeeeee/cccccc/200x150.png?text=No%20Image";
+          break;
+      }
+    },
   },
 };
 </script>
