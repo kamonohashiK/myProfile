3b2c15d2888670d53904f70b8e4c21e3fefe2647
門屋弘毅 <>
2021-07-25 11:07:22 +0900

    山手線に口紅をゲーム　基本部分の実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app >
     <AppBar />
-    <v-content>
+    <v-main>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
+    </v-main>
     <Footer />
   </v-app>
 </template>
---
+++ pages/yamabeni.vue
@@ -0,0 +1,240 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h3>山手線に口紅を</h3>
+      <v-text-field
+        type="text"
+        label="解答を入力"
+        v-model="answer"
+        @change="pushAnswer"
+      />
+      <p v-if="error_message" style="color: red">{{ error_message }}</p>
+      <br />
+      <p>残り{{ restWords.length }}個</p>
+      <b>禁止ワード</b>
+      <span v-for="b in banned_words">{{ b }}　</span>
+
+      <br /><button @click="surrender">降参</button>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      themes: [
+        { name: "東京", kana: "とうきょう", answered: false, banned: false },
+        { name: "神田", kana: "かんだ", answered: false, banned: false },
+        { name: "秋葉原", kana: "あきはばら", answered: false, banned: false },
+        { name: "御徒町", kana: "おかちまち", answered: false, banned: false },
+        { name: "上野", kana: "うえの", answered: false, banned: false },
+        { name: "鶯谷", kana: "うぐいすだに", answered: false, banned: false },
+        { name: "日暮里", kana: "にっぽり", answered: false, banned: false },
+        {
+          name: "西日暮里",
+          kana: "にしにっぽり",
+          answered: false,
+          banned: false,
+        },
+        { name: "田端", kana: "たばた", answered: false, banned: false },
+        { name: "駒込", kana: "こまごめ", answered: false, banned: false },
+        { name: "巣鴨", kana: "すがも", answered: false, banned: false },
+        { name: "大塚", kana: "おおつか", answered: false, banned: false },
+        { name: "池袋", kana: "いけぶくろ", answered: false, banned: false },
+        { name: "目白", kana: "めじろ", answered: false, banned: false },
+        {
+          name: "高田馬場",
+          kana: "たかだのばば",
+          answered: false,
+          banned: false,
+        },
+        {
+          name: "新大久保",
+          kana: "しんおおくぼ",
+          answered: false,
+          banned: false,
+        },
+        { name: "新宿", kana: "しんじゅく", answered: false, banned: false },
+        { name: "代々木", kana: "よよぎ", answered: false, banned: false },
+        { name: "原宿", kana: "はらじゅく", answered: false, banned: false },
+        { name: "渋谷", kana: "しぶや", answered: false, banned: false },
+        { name: "恵比寿", kana: "えびす", answered: false, banned: false },
+        { name: "目黒", kana: "めぐろ", answered: false, banned: false },
+        { name: "五反田", kana: "ごたんだ", answered: false, banned: false },
+        { name: "大崎", kana: "おおさき", answered: false, banned: false },
+        { name: "品川", kana: "しながわ", answered: false, banned: false },
+        {
+          name: "高輪ゲートウェイ",
+          kana: "たかなわげーとうぇい",
+          answered: false,
+          banned: false,
+        },
+        { name: "田町", kana: "たまち", answered: false, banned: false },
+        {
+          name: "浜松町",
+          kana: "はままつちょう",
+          answered: false,
+          banned: false,
+        },
+        { name: "新橋", kana: "しんばし", answered: false, banned: false },
+        {
+          name: "有楽町",
+          kana: "ゆうらくちょう",
+          answered: false,
+          banned: false,
+        },
+      ],
+      words: [
+        "あ",
+        "い",
+        "う",
+        "え",
+        "お",
+        "ぁ",
+        "ぃ",
+        "ぅ",
+        "ぇ",
+        "ぉ",
+        "か",
+        "き",
+        "く",
+        "け",
+        "こ",
+        "が",
+        "ぎ",
+        "ぐ",
+        "げ",
+        "ご",
+        "さ",
+        "し",
+        "す",
+        "せ",
+        "そ",
+        "ざ",
+        "じ",
+        "ず",
+        "ぜ",
+        "ぞ",
+        "た",
+        "ち",
+        "つ",
+        "て",
+        "と",
+        "っ",
+        "だ",
+        "ぢ",
+        "づ",
+        "で",
+        "ど",
+        "な",
+        "に",
+        "ぬ",
+        "ね",
+        "の",
+        "は",
+        "ひ",
+        "ふ",
+        "へ",
+        "ほ",
+        "ば",
+        "び",
+        "ぶ",
+        "べ",
+        "ぼ",
+        "ぱ",
+        "ぴ",
+        "ぷ",
+        "ぺ",
+        "ぽ",
+        "ま",
+        "み",
+        "む",
+        "め",
+        "も",
+        "や",
+        "ゆ",
+        "よ",
+        "ゃ",
+        "ゅ",
+        "ょ",
+        "ら",
+        "り",
+        "る",
+        "れ",
+        "ろ",
+        "わ",
+        "を",
+        "ん",
+      ],
+      banned_words: [],
+      answer: "",
+      error_message: "",
+    };
+  },
+  mounted() {
+    this.banWord();
+  },
+  methods: {
+    pushAnswer() {
+      if (this.answer != "") {
+        this.themes.some((el) => {
+          if (
+            el.name == this.answer &&
+            el.answered == false &&
+            el.banned == false
+          ) {
+            console.log(el.name);
+            el.answered = true;
+
+            //禁止ワードの追加
+            this.banWord();
+            this.answer = "";
+            this.error_message = "";
+            return true;
+          } else if (el.name == this.answer && el.answered == true) {
+            this.error_message = "すでに出ている答えです。";
+            return true;
+          } else if (el.name == this.answer && el.banned == true) {
+            this.error_message = "禁止ワードが含まれています。";
+            return true;
+          } else {
+            this.error_message = "お題に合っていません。";
+          }
+        });
+      } else {
+          this.error_message = "";
+      }
+    },
+    surrender() {
+      var str = "";
+      this.themes.forEach((e) => {
+        str += e.name + ",";
+      });
+      alert(str);
+    },
+    //文字をNG登録
+    banWord() {
+      var rand = Math.floor(Math.random() * this.words.length);
+      var ng = this.words[rand];
+      this.words.splice(rand, 1);
+      this.banned_words.push(ng);
+
+      this.themes.forEach((el) => {
+        if (el.kana.includes(ng)) {
+          console.log(el.name);
+          el.banned = true;
+        }
+      });
+    },
+  },
+  computed: {
+    restWords() {
+      var tmp = this.themes.filter(
+        (el) => el.banned == false && el.answered == false
+      );
+      return tmp;
+    },
+  },
+};
+</script>
